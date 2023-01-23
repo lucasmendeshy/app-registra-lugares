285f11e199d4e62e7be64eaab69c102a4e350f08
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2023-01-23 08:47:46 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import {
-  SafeAreaView,
   StyleSheet,
   Text,
   TextInput,
@@ -21,6 +20,42 @@ export default function App() {
   const [status, requestPermission] = ImagePicker.useCameraPermissions();
   const [foto, setFoto] = useState();
 
+  /* State para a geolocalização */
+  const [minhaLocalizacao, setMinhaLocalizacao] = useState(null);
+  const [localizacao, setLocalizacao] = useState();
+
+  useEffect(() => {
+    async function obterLocalizacao() {
+      // Acessando o status da requisição de permissão de uso
+      const { status } = Location.requestForegroundPermissionsAsync();
+      // Acessando os dados de globalização
+      let localizacaoAtual = await Location.getCurrentPositionAsync({});
+
+      // Adicionando os dados ao state
+      setMinhaLocalizacao(localizacaoAtual);
+    }
+    obterLocalizacao();
+  }, []);
+
+  console.log(minhaLocalizacao);
+
+  const regiaoInicial = {
+    latitude: -10,
+    longitude: -55,
+    latitudeDelta: 40,
+    logintudeDelta: 40,
+  };
+
+  const marcarLocal = (event) => {
+    setLocalizacao({
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+      ...minhaLocalizacao.coords,
+    });
+    console.log(localizacao);
+  };
+
+  /* -----------------------------------------------------------------------  */
 
   /* Programação abaixo é dos recursos de câmera, tirar foto e da requisição de permissão de uso*/
   useEffect(() => {
@@ -42,18 +77,20 @@ export default function App() {
     setFoto(imagem.assets[0].uri);
   };
   return (
-     <>
-    <SafeAreaView style={estilos.viewSafe}>
-      <ScrollView style={estilos.scroll}>
+    <>
+      <View style={estilos.container}>
         <StatusBar barStyle="dark-content" />
-        <View style={estilos.container}>
+        <ScrollView style={estilos.scroll}>
           <Text style={estilos.titulo}>App 1 - Fotos de lugares visitados</Text>
           <TextInput style={estilos.input} placeholder="Título da foto/local" />
 
           <View style={estilos.viewFoto}>
-          {foto && (
-         <Image source={{ uri: foto }} style={{ width: 379, height: 250 }} />
-          )}
+            {foto && (
+              <Image
+                source={{ uri: foto }}
+                style={{ width: 379, height: 250 }}
+              />
+            )}
           </View>
 
           <Pressable style={estilos.botao} onPress={acessarCamera}>
@@ -61,27 +98,45 @@ export default function App() {
           </Pressable>
 
           <View style={estilos.viewMapa}>
-            <MapView style={estilos.map}
-            mapType="satellite"
-            userInterfaceStyle="dark" 
-            />
+            <MapView
+              style={estilos.map}
+              mapType="satellite"
+              userInterfaceStyle="dark"
+              region={localizacao ?? regiaoInicial}
+              onPress={(e) => {
+                setLocalizacao({
+                  latitudeDelta: 0.0922,
+                  longitudeDelta: 0.0421,
+                  latitude: minhaLocalizacao.coords.latitude,
+                  longitude: minhaLocalizacao.coords.longitude,
+                });
+                console.log(localizacao);
+              }}
+            >
+              {localizacao && (
+                <Marker
+                  coordinate={localizacao}
+                  title="Socorrooo!!" // O título do marcador
+                  // pinColor="purple" -> Muda a cor do marcador
+                  draggable // Adicionar isso permite que o marcador seja arrastável (reposicionado).
+                  onPress={(event) => {
+                    console.log(event.nativeEvent);
+                  }} // Exibindo no console.log as coordenadas
+                ></Marker>
+              )}
+            </MapView>
           </View>
 
-          <Pressable style={estilos.botao}>
+          <Pressable style={estilos.botao} onPress={marcarLocal}>
             <Text style={estilos.textoBotao}>Localizar no mapa</Text>
           </Pressable>
-
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+        </ScrollView>
+      </View>
     </>
   );
 }
 
 const estilos = StyleSheet.create({
-  viewSafe: {
-    flex: 1,
-  },
   container: {
     flex: 1,
     padding: 8,
@@ -130,6 +185,6 @@ const estilos = StyleSheet.create({
   },
   map: {
     width: 379,
-    height: 250
-  }
+    height: 250,
+  },
 });
